d33cabb29eb99bb39e499a650aa4c2fe1b338a33
NotLeonid <leokim060109@gmail.com>
2023-10-12 08:55:07 -0300

    Initial

---
+++ preload.js
@@ -0,0 +1,36 @@
+const fs=require("fs");
+const os=require("os");
+const path=require("path");
+
+window.addEventListener("DOMContentLoaded",()=>{
+var oldPreview=document.querySelector("#old");
+var newPreview=document.querySelector("#new");
+var name=document.querySelector("#cape");
+var file=document.querySelector("#file");
+var skins=path.join(os.homedir(),"AppData","Roaming",".minecraft","assets","skins");
+
+oldPreview.src=`${skins}\\${name.value.substring(0,2)}\\${name.value}?${new Date().getTime()}`;
+name.addEventListener("change",e=>{
+oldPreview.src=`${skins}\\${name.value.substring(0,2)}\\${name.value}?${new Date().getTime()}`;
+});
+file.addEventListener("change",e=>{
+newPreview.src=`${file.files[0].path}?${new Date().getTime()}`;
+});
+document.querySelector("#apply").addEventListener("dblclick",async e=>{
+await fs.copyFileSync(file.files[0].path,`${skins}\\${name.value.substring(0,2)}\\${name.value}`);
+oldPreview.src=`${skins}\\${name.value.substring(0,2)}\\${name.value}?${new Date().getTime()}`;
+});
+document.querySelector("#reset").addEventListener("dblclick",async e=>{
+await fs.unlinkSync(`${skins}\\${name.value.substring(0,2)}\\${name.value}`);
+oldPreview.src=`${skins}\\${name.value.substring(0,2)}\\${name.value}?${new Date().getTime()}`;
+});
+
+document.querySelectorAll(".tab").forEach(button=>{
+button.addEventListener("click",e=>{
+document.querySelector(`.tab.selected`).classList.remove("selected");
+document.querySelector(`[name="${e.target.name}"]`).classList.add("selected");
+document.querySelector(`.page.selected`).classList.remove("selected");
+document.querySelector(`#${e.target.name}`).classList.add("selected");
+});
+});
+});
